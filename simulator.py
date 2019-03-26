72f2dab1813c9e5769799c2bbdd1bdcdfd5e1355
dcw3 <dcw3@princeton.edu>
2019-03-26 14:32:49 -0400

    add simulate_with_eval

--- simulator.py
+++ simulator.py
@@ -7,8 +7,8 @@ class TerminalSimulator:
         self.agent = agent
 
     # return list of cumulative reward for each episode
-    def simulate(self, n_episodes, initial_state):
-        cum_rewards = [0] * n_episodes
+    def simulate(self, n_episodes, initial_state, no_learning=False):
+        episode_rewards = [0] * n_episodes
         mdp = self.mdp
         for i in range(n_episodes):
             action = self.agent.begin_episode(initial_state, mdp.n_states, mdp.n_actions)
@@ -17,8 +17,20 @@ class TerminalSimulator:
             while current_state not in mdp.terminal_states:
                 current_time += 1
                 reward, current_state = mdp.step(current_state, action, current_time)
-                action = self.agent.step(reward, current_state, current_time)
-                cum_rewards[i] += reward
+                if no_learning:
+                    action = self.agent.step_no_learn(reward, current_state, current_time)
+                else:
+                    action = self.agent.step(reward, current_state, current_time)
+                episode_rewards[i] += reward
             self.agent.end_episode()
 
-        return cum_rewards
+        return episode_rewards
+
+    def simulate_with_eval(self, n_episodes, initial_state, n_eval_episodes = 1000):
+        episode_rewards = [0] * n_episodes
+        evaluated_rewards = [0] * n_episodes
+        for i in range(n_episodes):
+            episode_rewards[i] = self.simulate(1, initial_state, no_learning=False)[0]
+            simulated_rewards = self.simulate(n_eval_episodes, initial_state, no_learning=True)
+            evaluated_rewards[i] = np.mean(simulated_rewards)
+        return episode_rewards, evaluated_rewards
