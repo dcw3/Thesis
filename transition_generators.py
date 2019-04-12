019731b1fa54f2704a21a71e1ded9c93db5a3ebd
dcw3 <dcw3@princeton.edu>
2019-04-12 14:58:17 -0400

    add simple_transitions

--- transition_generators.py
+++ transition_generators.py
@@ -4,12 +4,39 @@ import numbers
 # i.i.d.
 def dirichlet_transitions(alpha=1.0, n_states=1, n_actions=1):
     if isinstance(alpha, numbers.Real):
+        alpha = np.array([alpha] * n_states)
+    assert(len(alpha) == n_states)
+
+    transition_matrices = [np.random.dirichlet(alpha, n_states) for _ in range(n_actions)]
+
+    return transition_matrices
+
+# TODO: allow n_dest_states to be a function rather than a constant
+# n_dest_per_state is the number of states reachable in one step by each state
+# n_dest_per_action is the number of states reachable in one step by each state-action pair
+def simple_transitions(n_states=1, n_dest_per_action=None, n_dest_per_state=None, alpha=1.0, n_actions=1):
+    if isinstance(n_dest_per_action, numbers.Real):
+        n_dest_per_action = np.array([n_dest_per_action] * n_states)
+
+    if isinstance(n_dest_per_state, numbers.Real):
+        n_dest_per_state = np.array([n_dest_per_state] * n_states)
+
+    if isinstance(alpha, numbers.Real):
         alpha_arr = np.array([alpha] * n_states)
     else:
         alpha_arr = alpha
-    assert(len(alpha_arr) == n_states)
 
-    transition_matrices = [np.random.dirichlet(alpha_arr, n_states) for _ in range(n_actions)]
+    transition_matrices = [np.zeros((n_states, n_states)) for _ in range(n_actions)]
+    for s in range(n_states):
+        reachable_states = np.random.choice(n_states, size=n_dest_per_state[s], replace=False)
+        for a in range(n_actions):
+            dest_states = np.random.choice(reachable_states, size=n_dest_per_action[s], replace=False)
+            alph_array = np.array([alpha_arr[s]] * n_dest_per_action[s])
+            dest_probs = np.random.dirichlet(alph_array, 1)[0]
+            all_probs = np.zeros(n_states)
+            for i in range(n_dest_per_action[s]):
+                all_probs[dest_states[i]] = dest_probs[i]
+            transition_matrices[a][s] = all_probs
 
     return transition_matrices
 
